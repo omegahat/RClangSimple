b36ccf7b96f1bab742559e4f292ff0881ddba6d4
Duncan Temple Lang <duncan@r-project.org>
2016-07-04 10:03:01 -0700

    currently using where Rinternals.h, etc. is on my builds of R which are never installed

--- tests/register.R
+++ tests/register.R
@@ -1,9 +1,17 @@
 library(RCIndex)
 #library(XML)
 #xmlSourceFunctions("inst/doc/RClangSimple.Rdb")
+
+# Need more flexible way of finding the R include directory, but this works
+# for a local installation I use for developing R.
+#inc = sprintf("%s%sinclude", R.home(), .Platform$file.sep)
+
+inc = sprintf("-I%s%sinclude", R.home(), .Platform$file.sep)
+if(file.exists(inc)) {
+#inc = system("R CMD config --cppflags", intern = TRUE)
 f = genFunctionCollector()
 parseTU(system.file("exampleCode", "register.c", package = "RCIndex"),
-          args = sprintf("-I%s%sinclude", R.home(), .Platform$file.sep), f$update)
+          args = inc, f$update)
 fns = f$funcs()
 
 names(fns)
@@ -21,4 +29,6 @@ d$type
 sapply(p, isSEXP)
 
 isDotCall(fns$foo)
+
+}
 
