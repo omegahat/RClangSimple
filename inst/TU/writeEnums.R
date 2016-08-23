18c41352ae5edcbe412e3878d712ead179529246
Duncan Temple Lang <duncan@r-project.org>
2016-08-23 16:20:40 -0700

    generated the 3.5  version enums

--- inst/TU/writeEnums.R
+++ inst/TU/writeEnums.R
@@ -1,15 +1,21 @@
-# source("clang.R")
+source("clang.R")
+
 
 library(RCodeGen)
 
 version = c(3, 7)
 enums = e
 ids = grep("(^__|::)", names(enums), invert = TRUE, value = TRUE) # remove llvm:: , std::, __lx
-Rfilename = sprintf("../../R/z_enumDefs_%d.%d.R", version[1], version[2])
-con = file(Rfilename, "w")
-sink(con)
-cat("if(all(clangVersion() == c(", version[1], ", ", version[2], "))) {\n\n")
-invisible(lapply(enums[ids], outputEnum))
-cat("\n\n\n}\n\n")
-sink()
+code = lapply(enums[ids], function(x) c(makeEnumClass(x), "", "", "#####################################"))
+
+Rfilename = sprintf("../../R/a_enumDefs_%d.%d.R", version[1], version[2])
+#con = file(Rfilename, "w")
+cat("if(all(clangVersion(libclangVersion_Install) == c(", version[1], ", ", version[2], "))) {\n\n",
+    unlist(code),
+    "\n\n\n}\n\n",
+    file = Rfilename, sep = "\n")
+
+#sink(con)
+#invisible(lapply(enums[ids], outputEnum))
+#sink()
 
