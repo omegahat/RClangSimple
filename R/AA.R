169337a558756eb5f28f6735b40118f9da3ff718
Duncan Temple Lang <duncan@r-project.org>
2016-11-25 14:24:34 -0800

    make runtime version function optionally return the numbers rather than just the string

--- R/AA.R
+++ R/AA.R
@@ -2,16 +2,21 @@ clangVersion =
     #
     # Run time version
     #
-function()
-  .Call("R_clang_getClangVersion")
-
+function(asNums = FALSE)
+{    
+  ans = .Call("R_clang_getClangVersion")
+  if(asNums)
+    clangVersionNum(ans)
+  else
+    ans
+}
 
 clangVersionNum =
+    #  clangVersionNum("clang version 3.8.0-2ubuntu4 (tags/RELEASE_380/final)")
 function(version = clangVersion())
 {
- ans = gsub("^clang version ([0-9.]+) .*", "\\1", version)
- as.integer(strsplit(ans, "\\.")[[1]])[1:2]
-#  c(3, 7)
+   ans = gsub("^clang version ([0-9.]+)[- ].*", "\\1", version)
+   as.integer(strsplit(ans, "\\.")[[1]])[1:2]
 }
 
 
