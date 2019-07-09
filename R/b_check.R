bc475d489ba3442ff6a1fdcf1b0420bb89c21838
Duncan Temple Lang <duncan@r-project.org>
2019-07-09 16:37:57 -0700

    message if we don't have the enums

---
+++ R/b_check.R
@@ -0,0 +1,5 @@
+if(!exists("CXType_Int")) {
+    stop("We don't appear to have enumerations for this version of libclang ", libClangVersion_Install, ".  Please contact the package maintainer or see the code in the TU/ directory of this package")
+#    stop("failed to install")
+}
+
