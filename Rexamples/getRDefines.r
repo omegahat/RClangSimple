e13b3671ced8069784b0a98c11f389bf7f137bbb
Duncan Temple Lang <duncan@r-project.org>
2019-07-09 20:15:10 -0700

    updates

--- Rexamples/getRDefines.r
+++ Rexamples/getRDefines.r
@@ -1,15 +1,23 @@
 # Read the R header file Rinternals.h and get the preprocessor defines.
 library(RCIndex)
 tu = createTU("/Users/duncan/Projects/R-3.0-dev/src/include/Rinternals.h",
-                    includes = c("/Users/duncan/Projects/R-3.0-dev/build/include", "/Users/duncan/Projects/R-3.0-dev/src/include"))
+              includes = c("/Users/duncan/Projects/R-3.0-dev/build/include",
+                           "/Users/duncan/Projects/R-3.0-dev/src/include"),
+              options = CXTranslationUnit_DetailedPreprocessingRecord)
 
-# Currently empty.
-defs = getDefines(tu)
-getCursorTokens(defs$STRSXP)
+# Get all of the defines, including the system & compiler ones.
+rdefs = getDefines(tu)
+length(rdefs)
+
+# Restrict to those in the R header files.
+rdefs = getDefines(tu, "R-3.0-dev")
+length(rdefs)
+
+getCursorTokens(rdefs$STRSXP)
 
 
-SexpTypes = grep("SXP$", names(defs), value = TRUE)
-sapply(defs[SexpTypes], function(x) as.integer(getCursorTokens(x)["Literal"]))
+SexpTypes = grep("SXP$", names(rdefs), value = TRUE)
+sapply(rdefs[SexpTypes], function(x) as.integer(getCursorTokens(x)["Literal"]))
 
 
 
