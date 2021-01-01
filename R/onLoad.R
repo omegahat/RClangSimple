4c71e995cf07f746262dabb40f70379e83dced83
Duncan Temple Lang <duncan@r-project.org>
2020-12-31 16:56:32 -0800

    check at load-time whether the configuration and runtime libraries for libclang are the same

---
+++ R/onLoad.R
@@ -0,0 +1,9 @@
+
+.onLoad =
+function(...)
+{
+    cfg = clangVersionNum(libclangVersion_Install)
+    rt = clangVersion(TRUE)
+if( cfg[1] != rt[1] )
+    message("**** SERIOUS:  run-time version and the configuration version of libclang are different and this could lead to incorrect results or crashes!\n",  if(cfg[1] > rt[1]) "**** Perhaps you need to remove the .o files in src/ and re-install", "\n\n")
+}
