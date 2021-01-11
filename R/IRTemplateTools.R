88e6c32708fc353185e1554adaba6f3e2635d529
Duncan Temple Lang <duncan@r-project.org>
2021-01-11 07:30:09 -0800

    note

--- R/IRTemplateTools.R
+++ R/IRTemplateTools.R
@@ -1,8 +1,13 @@
 
 mkIRTemplate =
     #
+    # To create the text with the C code for the void *[] array:
     #  cat(mkIRTemplate(r[1:4]), sep = "\n")
     #
+    # To create a file and include header files:
+    #  mkIRTemplate(r, "/tmp/bob.c", "clang-c/Index.h")
+    #
+    #  Could add support for types to reference them also.
     #
     #
 function(routines, file = character(), headerFiles)
