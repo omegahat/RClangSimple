bc437f611cb2071c4707efd8342d588dc3b6352b
Duncan Temple Lang <duncan@r-project.org>
2022-12-31 18:04:52 -0800

    formatting of code

--- R/onLoad.R
+++ R/onLoad.R
@@ -4,6 +4,9 @@ function(...)
 {
     cfg = clangVersionNum(libclangVersion_Install)
     rt = clangVersion(TRUE)
-if( cfg[1] != rt[1] )
-    message("**** SERIOUS:  run-time version and the configuration version of libclang are different and this could lead to incorrect results or crashes!\n",  if(cfg[1] > rt[1]) "**** Perhaps you need to remove the .o files in src/ and re-install", "\n\n")
+
+    if( cfg[1] != rt[1] )
+        message("**** SERIOUS:  run-time version and the configuration version of libclang are different and this could lead to incorrect results or crashes!\n",
+                if(cfg[1] > rt[1])
+                    "**** If building from source, perhaps you need to remove the .o files in src/ and re-install", "\n\n")
 }
