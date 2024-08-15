200bc4abe8a549720c2706a5a9b2667c6b3aa365
Duncan Temple Lang <duncan@r-project.org>
2024-08-15 08:31:47 -0700

    Add variables to example C++ class and use in a method.

--- inst/exampleCode/class.cpp
+++ inst/exampleCode/class.cpp
@@ -1,7 +1,13 @@
 class Foo {
 
+protected:
+    int a;
+    static long ctr;
+    
 public:
-    static int doit();
+    static int doit(){
+        ctr = ctr + 1;
+    };
     virtual double bar(int x);
     virtual double bar(int x, double y);
 };
